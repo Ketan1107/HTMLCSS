7511aa70c30b7acccb1de9e8edc37cf562ab3f71
Ketan1107 <ketanchoudhary1347@gmail.com>
2025-07-09 11:03:35 +0530

    update code

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    
+    // 
+    
+//    a,h;
+//     cout<<"enter a number";
+//     cin>>a;
+//     cout<<"enter a number";
+//     cin>>h;
+
+//     cout<<0.5*a*h; int 
+
+// int a,h;
+// cout<<"enter a number";
+// cin>>a;
+// cout<<"enter a number";
+// cin>>h;
+// cout<<2*3.14*a*h+2*3.14*a*h;
+
+// int l,w;
+// cout<<"enter a number";
+// cin>>l;
+// cout<<"enter a number";
+// cin>>w;
+// cout<<l*w;
+
+int a;
+cout<<"enter a number";
+cin>>a;
+cout<<3.14*a*a;
+
+
+
+
+
+}
