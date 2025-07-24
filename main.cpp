ec488ed3b21cc6afc95094497ac9419b431dae41
Ketan1107 <ketanchoudhary1347@gmail.com>
2025-07-24 10:12:07 +0530

    update code

--- main.cpp
+++ main.cpp
@@ -56,10 +56,85 @@ int main() {
 // cout<<(b>c && b>c)<<endl;
 // cout<<(c>a && c>b)<<endl;
 
-int a=2
+
+// int a =1234;
+// int b = 4321;
+// (a>b)?cout<<a: cout<<b;
+
+// int a= 18;
+// (a>=18)? cout<<"yes":cout<<"no";
+// cin>>a;
+
+
+// 
+
+
+// int number;
+// cin>>"enter a number";
+// (number==a)?cout<<"wecome":cout<<"incorrect":
+
+
+// int number;
+// int a=1234;
+// cin>>a;
+// (number==a)?cout<<"welcome":cout<<"incorrect";
+
+// int number;
+// cin>>number;
+// (number>=33)?cout<<"pass":cout<<"fail";
+
+
+
+// 
 
 
 
 
+// int number;
+// int a=10;
+// cin>>a;
+// (a%2==0)?cout<<"yes":cout<<"no";
+
+
+
+// int number;
+// int a=1234;
+// cin>>number;
+// cout<<"enter a number:";
+
+// if(number==a){
+//     cout<<"welcome";
+// }
+
+// else{
+//     cout<<"incorrect";
+// }
+
+// }
+
+
+
+// int number;
+// int a;
+// cout<<"enter a number:";
+// cin>>number;
+
+// if(number>=33){
+//     cout<<"pass";
+// }else{
+
+//     cout<<"fail";
+// }
+
+
+
+int number;
+cout<<"enter a number:";
+cin>>number;
+if(number%2==0){
+    cout<<"yes";
+}else{
+    cout<<"no";
+}
 
 }
